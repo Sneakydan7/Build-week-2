e81e676dce5392d0ebb2348981f34f47413483d3
Daniele Cagnoni <daniele.010@hotmail.it>
2023-11-16 17:41:52 +0100

    wip

--- scriptAlbum.js
+++ scriptAlbum.js
@@ -2,22 +2,20 @@ const params = new URLSearchParams(window.location.search);
 const itemId = params.get("_id");
 const URL = "https://deezerdevs-deezer.p.rapidapi.com/album/" + itemId;
 
-window.onload = () => {
-  fetch(URL, {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": "324d031408msh0f6aa06c1f35eb0p17c00fjsne3d27a65a55b",
-      "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
-    },
+fetch(URL, {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "324d031408msh0f6aa06c1f35eb0p17c00fjsne3d27a65a55b",
+    "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
+  },
+})
+  .then((resp) => {
+    if (!resp) {
+      throw new Error();
+    }
+    return resp.json();
   })
-    .then((resp) => {
-      if (!resp) {
-        throw new Error();
-      }
-      return resp.json();
-    })
-    .then((albumObj) => {
-      console.log(albumObj);
-    })
-    .catch((error) => Error(error));
-};
+  .then((albumObj) => {
+    console.log(albumObj);
+  })
+  .catch((error) => Error(error));
--- scriptArtist.js
+++ scriptArtist.js
@@ -2,20 +2,18 @@ const params = new URLSearchParams(window.location.search);
 const itemId = params.get("_id");
 const URL = "https://deezerdevs-deezer.p.rapidapi.com/artist/" + itemId;
 
-window.onload = () => {
-  fetch(URL, {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": "502b14b9demsh6c69f58b54e3ac4p1edca2jsn4ad89cccab70",
-      "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com"
+fetch(URL, {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "502b14b9demsh6c69f58b54e3ac4p1edca2jsn4ad89cccab70",
+    "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
+  },
+})
+  .then((resp) => {
+    if (!resp) {
+      throw new Error();
     }
+    return resp.json();
   })
-    .then((resp) => {
-      if (!resp) {
-        throw new Error();
-      }
-      return resp.json();
-    })
-    .then()
-    .catch();
-};
+  .then()
+  .catch();
