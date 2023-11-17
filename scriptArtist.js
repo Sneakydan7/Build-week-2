b557c0bd489d7e176989d3faa382b838fb1ceb56
Alessia Palmieri <146003639+palmieri24@users.noreply.github.com>
2023-11-17 03:37:33 +0100

    wip mobile

--- scriptArtist.js
+++ scriptArtist.js
@@ -10,8 +10,8 @@ fetch(URL, {
   method: "GET",
   headers: {
     "X-RapidAPI-Key": "502b14b9demsh6c69f58b54e3ac4p1edca2jsn4ad89cccab70",
-    "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
-  },
+    "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com"
+  }
 })
   .then((resp) => {
     if (!resp) {
@@ -24,6 +24,14 @@ fetch(URL, {
 
     let artistTitle = document.querySelector(".artist-cover-title");
     artistTitle.innerText = `${artistObj.name}`;
+
+    let imgBadge = document.querySelector(".artist-img-badge");
+    imgBadge.innerHTML = `<img
+          src="${artistObj.picture_small}"
+          class="card-img-top"
+          alt="..."
+          style="border-radius: 5px"
+        />`;
   })
   .catch((error) => Error(error));
 
@@ -31,8 +39,8 @@ fetch(urlTracklist, {
   method: "GET",
   headers: {
     "X-RapidAPI-Key": "502b14b9demsh6c69f58b54e3ac4p1edca2jsn4ad89cccab70",
-    "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
-  },
+    "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com"
+  }
 })
   .then((resp) => {
     if (!resp) {
@@ -42,5 +50,41 @@ fetch(urlTracklist, {
   })
   .then((trackObj) => {
     console.log(trackObj);
+    trackObj.data.forEach((e) => {
+      let popularSongs = document.querySelector(".popular-playlist-songs");
+
+      const row = document.createElement("div");
+      row.classList.add(
+        "row",
+        "popular-songs-list",
+        "pt-1",
+        "ms-1",
+        "ms-lg-4",
+        "mt-4",
+        "d-flex",
+        "align-items-center",
+        "mt-sm-1"
+      );
+
+      let trackMinutes = Math.floor(e.duration / 60);
+      let trackSeconds = e.duration - trackMinutes * 60;
+
+      row.innerHTML = `
+    <div class="col-5 mx-1 d-flex align-items-center text-center text-nowrap">
+      <img
+        src=${e.album.cover_small}
+        alt=""
+      />
+      <p class="m-1 p-2">${e.title}</p>
+    </div>
+    <div class="col-3 text-end">
+      <p class="m-1 p-2 ">${e.rank}</p>
+    </div>
+    <div class="col-3 text-end flex-grow-1 d-sm-none">
+      <p class="m-1 p-2 ">${trackMinutes}:${trackSeconds}</p>
+    </div>
+  </div>`;
+      popularSongs.appendChild(row);
+    });
   })
   .catch((error) => Error(error));
